6a4894bc319d9031f717baa64f05ac984e16b46e
Edgar Mijares <progresssystemsoftware@gmail.com>
2019-05-02 19:06:12 -0500

    Actualizar estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PrincipalComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
