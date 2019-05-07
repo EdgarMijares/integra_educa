658b573e7c6ab47cad7ceb6e03b01c069deda486
Edgar Mijares <progresssystemsoftware@gmail.com>
2019-05-07 08:52:29 -0500

    Instalar modulos de Firase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDWoXUb1WKIgk5tPhluvyZ9VOuIryLR5VA",
+    authDomain: "durango-integra.firebaseapp.com",
+    databaseURL: "https://durango-integra.firebaseio.com",
+    projectId: "durango-integra",
+    storageBucket: "durango-integra.appspot.com",
+    messagingSenderId: "584279252215",
+    appId: "1:584279252215:web:416a66cee9ca440f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDWoXUb1WKIgk5tPhluvyZ9VOuIryLR5VA",
+    authDomain: "durango-integra.firebaseapp.com",
+    databaseURL: "https://durango-integra.firebaseio.com",
+    projectId: "durango-integra",
+    storageBucket: "durango-integra.appspot.com",
+    messagingSenderId: "584279252215",
+    appId: "1:584279252215:web:416a66cee9ca440f"
+  }
 };
 
 /*
